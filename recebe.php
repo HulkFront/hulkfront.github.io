c4a07b8e99477134f94bf72e01c4643719cd52b5
Glauber <glauber.araujo@edu.pucrs.br>
2020-03-21 20:26:57 -0300

    Arquivos

---
+++ recebe.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>.::Programação Web::.</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<div class="topo">
+		<img src="img/topo.png" alt="topo">
+	</div>
+	<br><br><br><br><br>
+	<h1>TECTREINAMENTOS - TRATAMENTO DE DADOS COM PHP</h1><hr><br>
+
+	<div class="pesquisa">
+		<h6>Glauber Araújo</h6>
+	</div>
+
+	<div class="conteudo">
+		Aqui vai o primeiro conteudo<br>
+		Aqui vai o segundo conteudo<br>
+	</div>
+
+	<div class="rodape">
+		<h2>TECTREINAMENTOS - PROGRAMAÇÃO WEB COM PHP</h2><br>
+		<h2>PÁGINA DE TRATAMENTO DE DADOS</h2>
+	</div>
+	<footer>
+		<h3>Todos os direitos reservados - Desenvolvido por TecTreinamentos</h3>
+	</footer>
+
+</body>
+</html>
